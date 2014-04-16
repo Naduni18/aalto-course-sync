980437363b943661900885ee77ec784277f3c799
Lari Lehtomäki <lari@lehtomaki.fi>
2014-04-16 16:44:10 +0300

    Jostain syystä ei toimiva versio
    Extracted from a subdirectory of a course repository using
    git-blame-preserving git filter-branch hackery.

--- src/fi/aalto/t110_5140/group07_2014/NoppaSyncAdapter.java
+++ src/fi/aalto/t110_5140/group07_2014/NoppaSyncAdapter.java
@@ -1,5 +1,18 @@
 package fi.aalto.t110_5140.group07_2014;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
 import android.accounts.Account;
 import android.content.AbstractThreadedSyncAdapter;
 import android.content.ContentProviderClient;
@@ -16,10 +29,12 @@ import android.provider.CalendarContract.Calendars;
 import android.provider.CalendarContract.Events;
 import android.text.format.Time;
 import android.util.Log;
+import android.util.Xml;
 
 public class NoppaSyncAdapter extends AbstractThreadedSyncAdapter {
 
 	ContentResolver contentResolver;
+	String apiKey = "<removed>";		// Testi avain
 
 	public NoppaSyncAdapter(Context context, boolean autoInitialize) {
 		super(context, autoInitialize);
@@ -45,6 +60,21 @@ public class NoppaSyncAdapter extends AbstractThreadedSyncAdapter {
 			SyncResult result) {
 		
 		Log.d("NoppaSyncAdapter", "onPerformSync(...)");
+		
+		String[] courses = new String[] {
+				"T-86.5310",
+				"T-61.5010",
+				"T-106.5150",
+				"T-111.5360",
+				"T-110.6220",
+				"T-75.4300",
+				"T-75.4400",
+				"T-110.5140"
+		};
+		
+		/*
+		 * Create calendar
+		 */
 		
 		Uri calendarUri;
 		long calendarId = -1;
@@ -98,25 +128,184 @@ public class NoppaSyncAdapter extends AbstractThreadedSyncAdapter {
 						new String[]{ String.valueOf(calendarId) }) +
 				" old events deleted");
 		
-		// add a new event
-		ContentValues val = new ContentValues();
-		long start = System.currentTimeMillis() + 10*60*60*1000;
-		val.put(Events.TITLE, "nyt + 10h");
-		val.put(Events.DTSTART, start);
-		val.put(Events.DTEND, start + 30*60*1000); // +30min
-		val.put(Events.EVENT_TIMEZONE, Time.getCurrentTimezone());
-		val.put(Events.CALENDAR_ID, calendarId);
-		contentResolver.insert(
-				NoppaSyncAdapter.asSyncAdapter(Events.CONTENT_URI, account),
-				val);
+		/*
+		 * Fetch events
+		 */
 		
-		// uncomment to delete calendars created by this program:
-//		Log.i("NoppaSyncAdapter",
-//				"" +
-//				contentResolver.delete(
-//						Calendars.CONTENT_URI,
-//						Calendars.NAME + "=?", new String[]{ "testikalenteri" }) +
-//				" calendars deleted");
+		for (String course : courses) {
+
+			URL url = null;
+			InputStream in = null;
+			String title = "", type = "", description = "", location = "", start_time = "", end_time = "", start_date = "", end_date = "";
+			
+			try {
+				url = new URL("http://noppa-api-dev.aalto.fi/api/v1/courses/" + course + "/events.xml?key=" + apiKey);
+				// url = new URL("http://noppa-api-dev.aalto.fi/api/v1/courses/T-75.4400/events.xml?key=<removed>");
+			} catch (MalformedURLException e) {
+				Log.d("NoppaSyncAdapter", "MalformedURLException");
+			}
+	
+			HttpURLConnection urlConnection = null;
+			try {
+				Log.d("NoppaSyncAdapter", "HTTP Get API request, url: " + url.toString());
+				urlConnection = (HttpURLConnection) url.openConnection();
+				in = new BufferedInputStream(urlConnection.getInputStream());
+				
+			} catch (IOException e) {
+				Log.d("NoppaSyncAdapter", "IOException in url.openConnection or .getInputStream");
+			} finally {
+				urlConnection.disconnect();
+			}
+			
+			try {
+				XmlPullParser parser = Xml.newPullParser();
+				parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+				parser.setInput(in, null);
+				parser.nextTag();
+				Log.d("NoppaSyncAdapter", parser.getName());
+				
+				parser.require(XmlPullParser.START_TAG, null, "events");
+				while (parser.next() != XmlPullParser.END_DOCUMENT) {
+					if (parser.getEventType() != XmlPullParser.START_TAG) {
+						continue;
+					}
+					String name = parser.getName();
+					Log.d("NoppaSyncAdapter", "<" + parser.getName() + ">");
+	
+	
+					if (name.equals("title")) {
+						parser.require(XmlPullParser.START_TAG, null, "title");
+					    if (parser.next() == XmlPullParser.TEXT) {
+					        title = android.text.Html.fromHtml(parser.getText()).toString();
+					        parser.nextTag();
+					    }
+						parser.require(XmlPullParser.END_TAG, null, "title");
+					}
+					if (name.equals("type")) {
+						parser.require(XmlPullParser.START_TAG, null, "type");
+					    if (parser.next() == XmlPullParser.TEXT) {
+					    	Log.d("NoppaSyncAdapter", parser.getText());
+					    	if ( parser.getText().equals("event_course") ) {
+					    		type = "??";
+					    	} else if ( parser.getText().equals("exams") ) {
+					    		type = "Exam";
+					    	} else if ( parser.getText().equals("mid_term_exams") ) {
+					    		type = "Mid term exam";
+					    	} else if ( parser.getText().equals("other") ) {
+					    		type = "Other event";
+					    	} else if ( parser.getText().equals("seminar") ) {
+					    		type = "Seminar";
+					    	} else if ( parser.getText().equals("casework") ) {
+					    		type = "Case work";
+					    	} else if ( parser.getText().equals("demonstration") ) {
+					    		type = "Demonstration";
+					    	} else if ( parser.getText().equals("group_studies") ) {
+					    		type = "Group study";
+					    	} else if ( parser.getText().equals("individual_studies") ) {
+					    		type = "Individual study";
+					    	} else if ( parser.getText().equals("hybrid_studies") ) {
+					    		type = "Hybrid study";
+					    	} else if ( parser.getText().equals("online_studies") ) {
+					    		type = "Online study";
+					    	} else {
+					    		type = "???";
+					    	}
+					        parser.nextTag();
+					    }
+						parser.require(XmlPullParser.END_TAG, null, "type");
+					}
+					if (name.equals("location")) {
+						parser.require(XmlPullParser.START_TAG, null, "location");
+					    if (parser.next() == XmlPullParser.TEXT) {
+					    	location = android.text.Html.fromHtml(parser.getText()).toString();
+					        parser.nextTag();
+					    }
+						parser.require(XmlPullParser.END_TAG, null, "location");
+					}
+					if (name.equals("description")) {
+						parser.require(XmlPullParser.START_TAG, null, "description");
+					    if (parser.next() == XmlPullParser.TEXT) {
+					    	description = android.text.Html.fromHtml(parser.getText()).toString();
+					        parser.nextTag();
+					    }
+						parser.require(XmlPullParser.END_TAG, null, "description");
+					}
+					if (name.equals("start_date")) {
+						parser.require(XmlPullParser.START_TAG, null, "start_date");
+					    if (parser.next() == XmlPullParser.TEXT) {
+					    	start_date = parser.getText();
+					        parser.nextTag();
+					    }
+						parser.require(XmlPullParser.END_TAG, null, "start_date");
+					}
+					if (name.equals("end_date")) {
+						parser.require(XmlPullParser.START_TAG, null, "end_date");
+					    if (parser.next() == XmlPullParser.TEXT) {
+					    	end_date = parser.getText();
+					        parser.nextTag();
+					    }
+						parser.require(XmlPullParser.END_TAG, null, "end_date");
+					}
+					if (name.equals("start_time")) {
+						parser.require(XmlPullParser.START_TAG, null, "start_time");
+					    if (parser.next() == XmlPullParser.TEXT) {
+					    	start_time = parser.getText();
+					        parser.nextTag();
+					    }
+						parser.require(XmlPullParser.END_TAG, null, "start_time");
+					}
+					if (name.equals("end_time")) {
+						parser.require(XmlPullParser.START_TAG, null, "end_time");
+					    if (parser.next() == XmlPullParser.TEXT) {
+					    	end_time = parser.getText();
+					        parser.nextTag();
+					    }
+						parser.require(XmlPullParser.END_TAG, null, "end_time");
+					}
+				}
+	    
+			} catch (XmlPullParserException e) {
+			} catch (IOException e) {
+			} finally {
+				try {
+					in.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+	
+	
+			// add a new event
+			ContentValues val = new ContentValues();
+			long start = 0, end = 0;
+			try {
+	            DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+	            Log.i("NoppaSyncAdapter", "Date: " + start_date + " " + start_time);
+	            start = df.parse(start_date + " " + start_time).getTime();
+	            end = df.parse(end_date + " " + end_time).getTime();
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+					
+			val.put(Events.TITLE, title + " - " + type);
+			val.put(Events.DTSTART, start);
+			val.put(Events.DTEND, end);
+			val.put(Events.DESCRIPTION, description);
+			val.put(Events.EVENT_LOCATION, location);
+			val.put(Events.EVENT_TIMEZONE, Time.getCurrentTimezone());
+			val.put(Events.CALENDAR_ID, calendarId);
+			contentResolver.insert(
+					NoppaSyncAdapter.asSyncAdapter(Events.CONTENT_URI , account),
+					val);
+			
+			// uncomment to delete calendars created by this program:
+	//		Log.i("NoppaSyncAdapter",
+	//				"" +
+	//				contentResolver.delete(
+	//						Calendars.CONTENT_URI,
+	//						Calendars.NAME + "=?", new String[]{ "testikalenteri" }) +
+	//				" calendars deleted");
+		}
 	}
 	
 	private static Uri asSyncAdapter(Uri uri, Account account) {
